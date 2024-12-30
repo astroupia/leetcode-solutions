227d58f5aa0c90b6354f163e632dae5b4ac9d318
astroupia <103548612+astroupia@users.noreply.github.com>
2024-12-30 12:29:38 +0300

    724. Find Pivot Index.py

---
+++ 724. Find Pivot Index.py
@@ -0,0 +1,10 @@
+def pivotIndex(nums):
+        totalSum = sum(nums)
+        leftSum = 0
+        
+        for i in range(len(nums)):
+            if leftSum == totalSum - leftSum - nums[i]:
+                return i
+            leftSum += nums[i]
+        
+        return -1
